67697e9c8a94953cff49a680aafc5eb9cf797025
OswaldDeSwalking <108128410+OswaldDeSwalking@users.noreply.github.com>
2023-09-07 13:43:29 +0300

    Calculation 0.3v -add Result vue -add Logic Esay Calc

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
-function Ivoce (id)
-{
-   let inputTemp = document.querySelector('input');
-   inputTemp.value+=document.getElementById(`${id}`).innerHTML;
+function Ivoce(id) {
+    let inputTemp = document.querySelector('input');
+    if (id != "C") {
+        inputTemp.value += document.getElementById(`${id}`).innerHTML;
+    } else {
+        let splits = inputTemp.value.split("");
+        splits.pop();
+        let lav = splits.toString();
+        lav = lav.replaceAll(',', '')
+        inputTemp.value = lav;
+    }
+}
+
+function Result() {
+    let inputTemp = document.querySelector('input');
+    const regex = /[/*+-]/gm;
+    let simbol = inputTemp.value.match(regex);
+    if (simbol.length < 2) {
+        StandartCalc(simbol,inputTemp);
+    } else {
+        HardCalc(simbol,inputTemp);
+    }
+
+    function StandartCalc(sim,tepm)
+    {
+        var ssr = sim[0].toString();
+        let splits = inputTemp.value.split(sim[0].toString());
+        switch (ssr) {
+            case '/':
+                document.querySelector('label').innerHTML= parseInt(splits[0]) / parseInt(splits[1]);
+                tepm.value="";
+                break
+            case '*':
+                document.querySelector('label').innerHTML=(parseInt(splits[0]) * parseInt(splits[1]));
+                tepm.value="";
+                break
+            case '+':
+                document.querySelector('label').innerHTML=(parseInt(splits[0]) + parseInt(splits[1]));
+                tepm.value="";
+                break
+            case '-':
+                document.querySelector('label').innerHTML=(parseInt(splits[0]) - parseInt(splits[1]));
+                tepm.value="";
+                break
+        }
+        function HardCalc(sim)
+        {
+
+        }
+    }
 }
